942a9a3ddf991fe41ba4c3bb98bcde3ebdd6eb01
Adam Kalman <akalman@daptiv.com>
2017-03-10 14:38:10 -0800

    fill out rest of data but not screenshot

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
 let express = require('express');
 let path = require('path');
 let bodyParser = require('body-parser');
+let wreck = require('wreck');
+let sanitize = require('sanitize-html');
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.post('/sendbug', function(req, res){
-  console.log('recieved request');
-  console.log(req.body);
-  res.json(req.body);
+app.post('/sendbug', function(req, res) {
+  let payload = {
+    Name: req.body.title,
+    Description: formatDescription(req.body),
+    Project: { Id: 140294 },
+    Feature: getFeatureRef(req.body.category),
+    Tags: req.body.type
+  };
+
+  console.log(payload);
+
+  wreck.post(
+    'https://daptiv.tpondemand.com/api/v1/Bugs',
+    {
+      headers: {
+        'Authorization': 'Basic xxx',
+        'Content-Type': 'application/json'
+      },
+      payload: payload
+    },
+    function() { }
+  );
+
+  res.json({});
 });
+
+function sanitizish(dirty) {
+  return sanitize(dirty, {
+    allowedTags: sanitize.defaults.allowedTags.concat([ 'img' ]),
+    allowedSchemes: sanitize.defaults.allowedSchemes.concat([ 'data' ])
+  });
+}
+
+function getFeatureRef(category) {
+  switch (category) {
+    case 'UI':
+      return { Id: 140296 };
+    case 'Other':
+      return { Id: 140304 };
+    default:
+      throw new Error();
+  }
+}
+
+function getScreenshotElem(dataUrl) {
+  return `<img src="${dataUrl}" />`;
+}
+
+function formatDescription(body) {
+  // let screenshotUrl = getScreenshotElem(body.screenshot);
+  let descParts = [ body.url, body.description, /*screenshotUrl,*/ body.name];
+  let rawString = descParts.join('\n\n');
+  let midDesc = sanitizish(rawString)
+    .replace(/\n/g, '</div><div>')
+    .replace(/<div><\/div>/g, '<div>&nbsp;</div>');
+
+  return '<div>' + midDesc + '</div>';
+}
 
 app.listen(3000);
--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 sendBug = function() {
   let title = document.getElementById('title').value;
-  let type = document.getElementById('type').querySelector('input[name="type"]:checked').value;
   let category = document.getElementById('category').value;
+  let url = document.getElementById('url').value;
+  let type = document.getElementById('type').querySelector('input[name="type"]:checked').value;
   let description = document.getElementById('description').value;
+  let screenshot = document.getElementById('screenshot-data').src;
+  let name = document.getElementById('name').value;
 
   let data = {
     title: title,
-    type: type,
     category: category,
-    description: description
+    url: url,
+    type: type,
+    description: description,
+    screenshot: screenshot,
+    name: name
   };
 
   console.log(data);
@@ -17,3 +23,19 @@ sendBug = function() {
   xmlhttp.setRequestHeader("Content-Type", "application/json");
   xmlhttp.send(JSON.stringify(data));
 }
+
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('#screenshot').files[0];
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
+setInterval(previewFile, 500);
